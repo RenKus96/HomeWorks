c999d56bcad448ff560ed20cca10c73f50652aa1
RenKus <79753231+RenKus96@users.noreply.github.com>
2021-05-24 15:24:04 +0300

    Add files via upload

--- sc2_hw2.py
+++ sc2_hw2.py
@@ -2,38 +2,39 @@ from flask import Flask
 from faker import Faker
 import csv
 
+INCH_TO_CM = 2.54
+POUND_TO_KG = 0.453592
+
 app = Flask(__name__)
 
 @app.route("/avr_data")
 def get_avr_data():
     with open("hw.csv", encoding='utf-8') as r_file:
-        file_reader = csv.reader(r_file, delimiter = ",")
+        file_reader = csv.DictReader(r_file, delimiter = ",")
         out_str = '<h3> Список студентов из файла hw.csv:</h3>'
         out_str += '<p>'
         count = 0
         height_sum = 0.0
         weight_sum = 0.0
         for row in file_reader:
-            if count > 0:
-                height_sum += float(row[1])
-                weight_sum += float(row[2])
+            # out_str += '{}. {}<br>'.format(count+1,row)
+            height_sum += float(row[' "Height(Inches)"'])
+            weight_sum += float(row[' "Weight(Pounds)"'])
             count += 1
-        # отнимаем от счётчика единицу, что бы получить общее количество студентов (минусуем первую строку с заголовками)
-        count -= 1 
         out_str += ' Общее количество студентов - {}<br>'.format(count)
-        out_str += ' Средний рост: в дюймах - {:.2f}, в сантиметрах - {:.1f}<br>'.format(height_sum/count, height_sum*2.54/count)
-        out_str += ' Средний вес: в фунтах - {:.2f}, в килограммах - {:.2f}'.format(weight_sum/count, weight_sum*0.453592/count)
+        out_str += ' Средний рост: в дюймах - {:.2f}, в сантиметрах - {:.1f}<br>'.format(height_sum/count, height_sum*INCH_TO_CM/count)
+        out_str += ' Средний вес: в фунтах - {:.2f}, в килограммах - {:.2f}'.format(weight_sum/count, weight_sum*POUND_TO_KG/count)
         out_str += '</p>'
     return out_str
 
 @app.route("/requirements")
 def get_requirements():
-    f_req = open('requirements.txt')
     out_str = '<h3> Содержимое файла requirements.txt:</h3>'
     out_str += '<p>'
-    for cnt,module in enumerate(f_req):
-        out_str += '{}. {}<br>'.format(cnt+1,module)
-    out_str += '</p>'
+    with open('requirements.txt') as f_req:
+        for cnt,module in enumerate(f_req, 1):
+            out_str += '{}. {}<br>'.format(cnt, module)
+        out_str += '</p>'
     return out_str
 
 @app.route("/random_students")
